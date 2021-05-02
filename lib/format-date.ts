3e5f3d9d7f50ebd98a70cccdb964ae43542bec57
Thomas Prochazka <thomas.prochazka@gmail.com>
2021-05-02 10:26:44 +0200

    Hide build error due to TS types

--- lib/format-date.ts
+++ lib/format-date.ts
@@ -1,4 +1,7 @@
-const dateFormatter = new Intl.DateTimeFormat([], { dateStyle: "long" });
+const dateFormatter = new Intl.DateTimeFormat([], {
+  dateStyle: "long",
+  // as any to intentionally hide the build error. https://github.com/microsoft/TypeScript/issues/38266
+} as any);
 
 export const formatDate = (date: Date) => {
   return dateFormatter.format(date);
