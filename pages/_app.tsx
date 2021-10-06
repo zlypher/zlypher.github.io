002d5ce682d673670ca81600b4a5ff69615dc92a
Thomas Prochazka <thomas.prochazka@gmail.com>
2021-10-06 22:09:59 +0200

    Update Next and add Splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import { IconSpritesheet } from "../components/icon-spritesheet";
+import Script from "next/script";
 import "../styles/main.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script src="https://cdn.splitbee.io/sb.js" />
       <IconSpritesheet />
       <Component {...pageProps} />
     </>
