1129e37921605f4f5317d828a857288f9cf2cfe0
zlypher <thomas.prochazka@gmail.com>
2021-10-06 19:32:21 +0000

    Deploying to gh-pages from  @ 978a88eee7ce2fc6b86093e77c58182755e9d3d6 🚀

--- lib/format-date.ts
+++
@@ -1,8 +0,0 @@
-const dateFormatter = new Intl.DateTimeFormat([], {
-  dateStyle: "long",
-  // as any to intentionally hide the build error. https://github.com/microsoft/TypeScript/issues/38266
-} as any);
-
-export const formatDate = (date: Date) => {
-  return dateFormatter.format(date);
-};
